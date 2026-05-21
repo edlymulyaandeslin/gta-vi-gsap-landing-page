89b41ddc4e0f431389cb79e2761b53d309cb5036
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-05-21 23:37:19 +0700

    feat: implement postcard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './sections/Hero';
 import Jason from './sections/Jason';
 import Lucia from './sections/Lucia';
 import Navbar from './sections/Navbar';
+import PostCard from './sections/PostCard';
 import SecondVideo from './sections/SecondVideo';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -21,6 +22,8 @@ const App = () => {
 
       <SecondVideo />
       <Lucia />
+
+      <PostCard />
     </main>
   );
 };
