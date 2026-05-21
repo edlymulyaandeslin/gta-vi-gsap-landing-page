7713546cbd29bfd09703991b2c997584f382bd25
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-05-22 00:42:35 +0700

    feat: implement outro

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './sections/Hero';
 import Jason from './sections/Jason';
 import Lucia from './sections/Lucia';
 import Navbar from './sections/Navbar';
+import Outro from './sections/Outro';
 import PostCard from './sections/PostCard';
 import SecondVideo from './sections/SecondVideo';
 
@@ -27,6 +28,8 @@ const App = () => {
       <PostCard />
 
       <Final />
+
+      <Outro />
     </main>
   );
 };
---
+++ src/sections/Outro.jsx
@@ -0,0 +1,45 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+const Outro = () => {
+  useGSAP(() => {
+    gsap.set('.final-message', { marginTop: '-100vh', opacity: 0 });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.final-message',
+        start: '80% bottom',
+        end: '80% bottom',
+        scrub: true,
+      },
+    });
+
+    tl.to('.final-content', { opacity: 0, duration: 1, ease: 'power1.inOut' });
+    tl.to('.final-message', { opacity: 1, duration: 1, ease: 'power1.inOut' });
+  });
+
+  return (
+    <section className="final-message">
+      <div className="h-full col-center gap-10">
+        <img src="/images/logo.webp" alt="logo" className="md:w-72 w-52" />
+
+        <div>
+          <h3 className="gradient-title">
+            Coming <br /> May 26th <br /> 2026
+          </h3>
+        </div>
+
+        <div className="flex-center gap-10">
+          <img
+            src="/images/ps-logo.svg"
+            alt="ps-logo"
+            className="md:w-32 w-20"
+          />
+          <img src="/images/x-logo.svg" alt="x-logo" className="md:w-52 w-40" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Outro;
